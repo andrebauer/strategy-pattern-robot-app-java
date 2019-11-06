da5d4d4792424e4910cd86088968b6736fc9a2b0
André Bauer <bauerand@informatik.uni-marburg.de>
2019-11-06 18:47:52 +0100

    Add Robot.

---
+++ src/main/java/robotapp/Robot.java
@@ -0,0 +1,18 @@
+package robotapp;
+
+public class Robot {
+  private String name;
+  private Behaviour behaviour;
+
+  public Robot(String name) {
+    this.name = name;
+  }
+
+  public void setMoveBehaviour(Behaviour behaviour) {
+    this.behaviour = behaviour;
+  }
+
+  public Move doMove(State state) {
+    return behaviour.getMove(state);
+  }
+}
